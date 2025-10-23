2f20f7e7a27bc2d387588f81a145e523f40010c8
PrRiccardo <riccard.prosperi@studenti.unicam.it>
2025-10-23 17:32:06 +0200

    Creazione progetto
    Creazione del progetto condiviso per la realizzazione di una piattaforma di digitalizzazione e valorizzazione della filiera agricola locale

---
+++ FilieraAgricola/src/main/java/unicam/filieraAgricola/FilieraAgricolaApplication.java
@@ -0,0 +1,13 @@
+package unicam.filieraAgricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraAgricolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilieraAgricolaApplication.class, args);
+    }
+
+}
---
+++ FilieraAgricola/src/test/java/unicam/filieraAgricola/FilieraAgricolaApplicationTests.java
@@ -0,0 +1,13 @@
+package unicam.filieraAgricola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilieraAgricolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
